1d1cd06b047f4c1a5d93b7378a00b96f1b2f8eac
iampr <iampr@PRABIN>
2023-01-03 23:37:34 +0545

    Add project files.

---
+++ asabeginner7(try parse)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string strnumber = "5652";
+        int result = 0;
+        bool isconversionSucessful = int.TryParse(strnumber, out result);
+        if (isconversionSucessful)
+        {
+            Console.WriteLine(result);
+        }
+       else
+        {
+            Console.WriteLine("it is invalid ");
+        }
+    }
+}
